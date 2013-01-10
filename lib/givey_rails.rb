166c3f22fe262d05d078ce19121bb8a097da716b
Philip Poots <philip.poots@gmail.com>
2013-01-10 11:33:39 +0000

    Include eu_central_bank for Money

--- lib/givey_rails.rb
+++ lib/givey_rails.rb
@@ -1,8 +1,9 @@
+require "eu_central_bank"
 require "givey_ruby"
 require "givey_rails/engine"
 require "givey_rails/routes"
 require "givey_rails/controller_helpers"
-require 'givey_rails/givey_model'
+require "givey_rails/givey_model"
 
 module GiveyRails
 
