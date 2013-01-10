0d2f28a96bc0ddf43fc512c69d0c3af98c22340f
Philip Poots <philip.poots@gmail.com>
2013-01-10 11:40:18 +0000

    Migrate Channel to GiveyModel

--- app/models/givey_rails/channel.rb
+++ app/models/givey_rails/channel.rb
@@ -1,36 +1,9 @@
 module GiveyRails
   class Channel
-    include ActiveModel::Validations
-    include ActiveModel::Conversion
-    extend  ActiveModel::Naming
-
-    attr_accessor :id,
-      :givey_tag,
-      :name,
-      :node_type,
-      :description
-
-    def initialize(attributes = {})
-      attributes.each do |name, value|
-        send("#{name}=", value)
-      end
-    end
-
-    def persisted?
-      !(self.id.nil?)
-    end
+    include GiveyModel
 
     def name_and_tag
       "#{givey_tag} - #{name}"
-    end
-
-    # RELATIONSHIPS
-    def charity
-      @charity
-    end
-
-    def charity=(charity_attributes)
-      @charity = charity_attributes ? Charity.new(charity_attributes) : nil
     end
   end
 end
