a3cdc240c7b789707a4c26b37b03860605dd3275
Philip Poots <philip.poots@gmail.com>
2013-01-10 11:38:50 +0000

    Migrate Address to GiveyModel

--- app/models/givey_rails/address.rb
+++ app/models/givey_rails/address.rb
@@ -1,39 +1,6 @@
 module GiveyRails
   class Address
-    include ActiveModel::Validations
-    include ActiveModel::Conversion
-    extend  ActiveModel::Naming
-
-    attr_accessor :id,
-      :address_name,
-      :first_name,
-      :last_name,
-      :address1,
-      :address2,
-      :address3,
-      :city,
-      :county,
-      :country,
-      :post_code,
-      :phone_number,
-      :node_type,
-      :created_at
-
+    include GiveyRails::GiveyModel
     #validates_presence_of :amount, :donation_id
-
-
-    def initialize(attributes = {})
-      attributes.each do |name, value|
-        send("#{name}=", value)
-      end
-    end
-
-    def persisted?
-      !(self.id.nil?)
-    end
-
-    def created_at
-      DateTime.parse(@created_at)
-    end
   end
 end
