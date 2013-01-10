c6170d548f8fc80c1170d57b22bda5f2295ff24a
Philip Poots <philip.poots@gmail.com>
2013-01-10 11:18:56 +0000

    Migrate Charity to GiveyModel with has_many

--- lib/givey_rails/givey_model.rb
+++ lib/givey_rails/givey_model.rb
@@ -13,6 +13,7 @@ module GiveyRails
         self.class.send(:attr_accessor, name.to_sym)
         value = DateTime.parse(value) if value.respond_to?(:match) && value.match(DATETIME_FORMAT).present?
         value = build_relationship(name, value) if value.kind_of? Hash
+        value = build_relationships(name, value) if value.kind_of? Array
         send("#{name}=", value)
       end
     end
@@ -25,9 +26,16 @@ module GiveyRails
 
     def build_relationship(name, attributes)
       return nil if attributes.empty?
-      name = "user" if [:giver_target].include?(name)
+      name = "user" if [:giver_target, :select_user].include?(name)
       name = "GiveyRails::" + name.classify
       name.constantize.new(attributes)
+    end
+
+    def build_relationships(name, value)
+      return nil if value.empty?
+      klass_name = [:supporters, :donors].include?(name.to_sym) ? "users" : name
+      klass_name = "GiveyRails::" + klass_name.singularize.classify
+      value.map {|attrs| klass_name.constantize.new(attrs[name.singularize]) }
     end
   end
 end
--- spec/models/givey_rails/charity_spec.rb
+++ spec/models/givey_rails/charity_spec.rb
@@ -8,5 +8,32 @@ module GiveyRails
         charity.name_and_tag.should == "HERO - Heroes"
       end
     end
+
+    describe "#build_relationships" do
+      let(:charity_response) do
+        ActiveSupport::JSON.decode(
+          File.read("#{ENGINE_RAILS_ROOT}/spec/fixtures/charity.json")
+        )
+      end
+
+      context "with three donors" do
+        describe "#donors" do
+          it "builds an array of three users" do
+            charity = Charity.new(charity_response)
+            charity.donors.should have(3).items
+            charity.donors.first.should be_kind_of(User)
+          end
+        end
+      end
+
+      context "with no supporters" do
+        describe "#supporters" do
+          it "returns nil" do
+            charity = Charity.new(charity_response)
+            charity.supporters.should be_nil
+          end
+        end
+      end
+    end
   end
 end
