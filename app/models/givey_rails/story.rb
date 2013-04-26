c0bf6fd38ddf4a5881b02fe453d213f8f19ba41a
Nick Reavill <nick@givey.com>
2013-04-26 14:32:21 +0100

    GIVEY-103 #comment add activity_asset to story

--- app/models/givey_rails/story.rb
+++ app/models/givey_rails/story.rb
@@ -9,6 +9,7 @@ module GiveyRails
       :user_thumb_url,
       :content_title,
       :content_text,
+      :activity_asset,
       :activity_asset_url,
       :activity_asset_square_url,
       :activity_asset_thumb_url,
