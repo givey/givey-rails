897359dc2a28dd9b942801d406f1424e33fa58dd
Nick Reavill <nick@givey.com>
2013-06-07 12:13:40 +0100

    GIVEY-321 #start #comment handle new channel fields

--- app/models/givey_rails/channel.rb
+++ app/models/givey_rails/channel.rb
@@ -13,10 +13,25 @@ module GiveyRails
       :donors,
       :donor_count,
       :supporters,
-      :supporter_count
+      :supporter_count,
+      :charity_donations_total
     
     def name_and_tag
       "#{givey_tag} - #{name}"
     end
+
+    def charity_donations_total_display 
+      Money.new(@charity_donations_total).cents / 100
+    end
+
+    # default currency
+    def currency
+      @currency.blank? ? 'GBP' : @currency
+    end
+
+    def display_currency
+      Money.new(0, currency).currency
+    end
+
   end
 end
--- spec/models/givey_rails/channel_spec.rb
+++ spec/models/givey_rails/channel_spec.rb
@@ -13,5 +13,23 @@ module GiveyRails
         channel.name_and_tag.should == "HELP - Help"
       end
     end
+
+    describe "default currency" do
+
+      it "has a default currency of GBP" do
+        channel = Channel.new
+        channel.currency.should == 'GBP'
+      end
+
+    end
+
+    describe "charity_donations_total_display" do
+
+      it "returns the correct amount" do
+        channel = Channel.new(charity_donations_total: 1400)
+        channel.charity_donations_total_display.should == 14
+      end
+
+    end
   end
 end
