4788400be1c449b6d3c965fd5900ecc289655b69
Philip Poots <philip.poots@gmail.com>
2013-11-22 13:37:49 +0000

    Revert "Don't bother replacing donation string"
    This reverts commit 4b84abbc8720fb7a1db735bdf9b7905c13e3c457.

--- app/models/givey_rails/donation.rb
+++ app/models/givey_rails/donation.rb
@@ -78,11 +78,25 @@ module GiveyRails
     end
 
     def donation_string_html
-      RedCloth.new(donation_string_textile, [:lite_mode]).to_html
+      RedCloth.new(replaced_str, [:lite_mode]).to_html
     end
 
     def donation_string_plain
       replaced_str.gsub(/\[[^\]]+:http:\/\/givey.com\/(\w+)\]/){|m| "##{$1}" }
+    end
+
+    def replaced_str
+      if donation_string_textile =~ /time-\d+-time/
+        str_amt       = donation_string_textile.match(/time-(\d+)-time/).captures[0].to_i
+        time_str      = time_donation_format(str_amt)
+        donation_string_textile.gsub(/time-\d+-time/, time_str)
+      elsif donation_string_textile =~ /amt-.+-amt/
+        str_cur, str_amt  = donation_string_textile.match(/amt-([a-z]{3}|[A-Z]{3})(\d+)-amt/).captures
+        money_object      = Money.new(str_amt, str_cur)
+        donation_string_textile.gsub(/amt-([a-z]{3}|[A-Z]{3})/, money_object.currency.symbol).gsub(/\d+-amt/, (money_object.cents / 100).to_s)
+      else
+        donation_string_textile
+      end
     end
 
     def donation_images_html
